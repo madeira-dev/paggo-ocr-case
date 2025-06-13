ad4233cfbe9c6887a6a0f264b4b434f3d98afb4a
Gabriel Madeira <gabrielmadeira2002@gmail.com>
2025-06-13 13:26:57 -0300

    update cors allowed origins

--- paggo-monorepo/apps/backend/src/main.ts
+++ paggo-monorepo/apps/backend/src/main.ts
@@ -1,10 +1,17 @@
 console.log("[Backend] main.ts execution started"); // debug
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MethodNotAllowedException } from '@nestjs/common';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    app.enableCors();
+    app.enableCors({
+        origin: [
+            'https://your-production-frontend-url.vercel.app',
+            'http://localhost:3001' // remove this later
+        ],
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    });
     const port = process.env.PORT || 3000;
     await app.listen(port);
     console.log(`Backend application running on: ${await app.getUrl()}`);
